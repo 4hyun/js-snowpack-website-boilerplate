7f767d44c495066bc4bbb338bc75d760eaaf0aaa
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-11 21:36:10 +0900

    use "public" env var: token

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
+import axios from 'axios';
 console.log('loaded!');
+
+/* Sample Code - Github Client */
+const { SNOWPACK_PUBLIC_GITHUB_TOKEN: token } = import.meta.env;
+const GithubClient = axios.create({
+  baseurl: '',
+  timeout: 1000,
+  headers: {
+    Accept: 'application/vnd.Github.v3+json',
+  },
+});
