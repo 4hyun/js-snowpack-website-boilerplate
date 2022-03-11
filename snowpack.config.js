d7e91dcc7a575c2bee85597b5861b27c7795e68f
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-11 21:01:43 +0900

    deps: use dotenv plugin

--- snowpack.config.js
+++ snowpack.config.js
@@ -5,12 +5,13 @@
 module.exports = {
   mount: {
     /* ... */
-    public: { url: "/", static: true },
-    src: { url: "/dist" },
+    public: { url: '/', static: true },
+    src: { url: '/dist' },
   },
   plugins: [
     /* ... */
-    "@snowpack/plugin-sass",
+    '@snowpack/plugin-dotenv',
+    '@snowpack/plugin-sass',
   ],
   packageOptions: {
     /* ... */
@@ -22,4 +23,4 @@ module.exports = {
     /* ... */
   },
   optimize: { minify: true },
-}
+};
