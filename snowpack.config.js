a228d2a5b85a6b426c950bf7c03511f74a545d5c
Sa Hyun Hwang <4hyun8@gmail.com>
2022-03-11 20:52:55 +0900

    save

---
+++ snowpack.config.js
@@ -0,0 +1,25 @@
+// Snowpack Configuration File
+// See all supported options: https://www.snowpack.dev/reference/configuration
+
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    /* ... */
+    public: { url: "/", static: true },
+    src: { url: "/dist" },
+  },
+  plugins: [
+    /* ... */
+    "@snowpack/plugin-sass",
+  ],
+  packageOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  optimize: { minify: true },
+}
